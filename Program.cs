1eb51528260509189fc15312f971d770663749f5
Савва Чальцев <cavvach@gmail.com>
2023-03-20 13:10:44 -0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Patterns_Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass myObject = new ImplementationOne(1);
+            myObject.GetId();
+
+            BaseClass clone = myObject.Clone();
+            clone.GetId();
+
+            myObject = new ImplementationTwo(2);
+            myObject.GetId();
+
+            clone = myObject.Clone();
+            clone.GetId();
+        }
+    }
+}
